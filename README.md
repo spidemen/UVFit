a0c9297b8448eb3db5c9472f75768f8b9dbc98f5
BILL <xing@BILLs-MacBook-Pro.local>
2018-11-10 00:01:57 -0700

    readme

--- README.md
+++ README.md
@@ -1,10 +1,17 @@
-﻿
 
 
 AWS Server IP: 18.222.203.101
 key file: was.pem
 
 Login: ssh -i was.pem ec2-user@18.222.203.101. (on the was.pem directory)
+
+start data: ./mongod
+Data Directory :  ../data
+
+look up data: mongon
+
+some command: show dbs;  show collections; db.$collections.find();
+
 
 node version: v10.12.0 above 10 is fine
 npm: 6.4.1
--- public/js/profile.js
+++ public/js/profile.js
@@ -2,7 +2,37 @@
 $("#register").click(function(){
 	  console.log("click button update ");
       $(".registerbox").css('display',"block");
+       $(".view").css('display',"none");
 });
+
+$("#viewData").click(function(){
+     console.log("click button update ");
+      $(".view").css('display',"block");
+      $(".registerbox").css('display',"none");
+
+
+});
+
+function sendReqStore()
+{
+
+  var xhr = new XMLHttpRequest();
+  xhr.addEventListener("load", RegisterRespon);
+  xhr.responseType = "json";
+  xhr.open("POST", '/test');
+  xhr.setRequestHeader("Content-type", "application/json");
+  console.log("store testing data");
+  xhr.send();
+
+}
+
+$("#storeData").click(function(){
+   
+       sendReqStore();
+
+});
+
+
 
 function sendReqRegister() {
     var email = document.getElementById("email").value;
@@ -12,10 +42,11 @@ function sendReqRegister() {
   var xhr = new XMLHttpRequest();
   xhr.addEventListener("load", RegisterRespon);
   xhr.responseType = "json";
-  xhr.open("POST", '/register');
+  xhr.open("POST", '/devices/register');
   xhr.setRequestHeader("Content-type", "application/json");
   console.log(email);
   xhr.send(JSON.stringify({email:email,deviceId:deviceId, deviceName:deviceName}));
+
 }
 
 function RegisterRespon(){
@@ -23,19 +54,17 @@ function RegisterRespon(){
 	var responseHTML="";
 	if (this.status === 201) {
     if (this.response.registered) {
-
       responseHTML = "<span>";
       responseHTML += "Success register: deviceId "+ this.response.deviceId;
       responseHTML += "</span>"	
-
-      // Change current location to the signin page.
+     // Change current location to the signin page.
      //  window.location = "index.html";
     } 
     else
     {
          // Use a span with dark red text for errors
       responseHTML = "<span>";
-      responseHTML += "Error: internal" ;
+      responseHTML += "Error: "+this.response.message;
       responseHTML += "</span>"
     }
   }
@@ -43,20 +72,71 @@ function RegisterRespon(){
   {
   	 // Use a span with dark red text for errors
     responseHTML = "<span>";
-    responseHTML += "Error: ";
+    responseHTML += "Error: "+this.response.message;
     responseHTML += "</span>"
   }
 
    $(".error").html(responseHTML);
-    $(".error").css('display',"block");
+   $(".error").css('display',"block");
 
 }
 
 $("#submit").click(function(){
 	 
-      sendReqRegister();
+       sendReqRegister();
 
 });
+
+
+
+
+
+function  sendReqViewData(){
+
+  var email = "demo@email.com";
+  var deviceId = "11f4baaef3445ff";
+  var xhr = new XMLHttpRequest();
+  xhr.addEventListener("load", ViewDataRespon);
+  xhr.responseType = "json";
+  xhr.open("GET", '/activities/user');
+  xhr.setRequestHeader("Content-type", "application/json");
+  console.log(email);
+  xhr.send(JSON.stringify({email:email,deviceId:deviceId}));
+
+};
+$("#viewbutton").click(function(){
+       sendReqViewData();
+});
+
+function ViewDataRespon(){
+    
+    if(this.status === 200||this.status==201)
+    {
+      var responseHTMLType="Activies Type:";
+      var responseHTMLDate="Date:";
+      var responseHTMLGPS="GPS location::";
+      var responseHTMLUV="UV data::";
+      for(var  data of this.response.activities)
+      {
+          responseHTMLType+="<p>"+data.type+"</p>";
+          responseHTMLDate+="<p>"+data.date+"</p>";
+          responseHTMLGPS+="<p>  "+data.lons+"  "+data.lats+" </p>";
+          responseHTMLUV+="<p>"+data.uv+"</p>"
+      }
+
+      $("#type").html(responseHTMLType);
+      $("#Date").html(responseHTMLDate);
+      $("#GPS").html(responseHTMLGPS);
+      $("#UV").html(responseHTMLUV);
+      console.log("view data");
+
+    }
+    else
+    {
+      console.log("Error: view data "+this.status);
+    }
+
+}
 
 
 
